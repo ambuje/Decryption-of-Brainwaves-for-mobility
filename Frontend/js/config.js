0b337393a416961d26fbfea409a14abd6b3d9a08
Akshita Mehta <44060710+MehtaAkshita@users.noreply.github.com>
2019-07-21 07:03:28 +0545

    Js

---
+++ Frontend/js/config.js
@@ -0,0 +1,17 @@
+host = '10.16.1.36';	// hostname or IP address
+port = 1884;
+topic = '#';		// topic to subscribe to
+useTLS = false;
+username = "username";
+password = "qwertyuiop";
+// username = "jjolie";
+// password = "aa";
+
+// path as in "scheme:[//[user:password@]host[:port]][/]path[?query][#fragment]"
+//    defaults to "/mqtt"
+//    may include query and fragment
+//
+// path = "/mqtt";
+// path = "/data/cloud?device=12345";
+
+cleansession = true;
